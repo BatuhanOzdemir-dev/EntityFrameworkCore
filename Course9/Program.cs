6c3b9f9982aa0b7507fbf3549b7f46fc4e4052c9
Batuhan Özdemir <batuhan22.ozdemir@gmail.com>
2023-12-15 17:40:30 +0300

    Add project files.

---
+++ Course9/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Course9;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        ETicaretContext context = new();
+
+        //Uncomment to seed data.
+        //SeedData(context);
+
+        Product? product1 = await context.Products.FirstOrDefaultAsync(p => p.Name == "Kapı1");
+        product1.Name = "Bob";
+        product1.Price = 400;
+
+        await context.SaveChangesAsync();
+
+    }
+
+    public async Task SeedData(ETicaretContext context)
+    {
+
+        List<Product> Products = new()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kapı1",
+                Price = 100,
+                Quantity = 20
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kapı2",
+                Price = 200,
+                Quantity = 8
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kapı3",
+                Price = 300,
+                Quantity = 2
+            }
+        };
+
+        await context.Products.AddRangeAsync(Products);
+        await context.SaveChangesAsync();
+    }
+}
+
+
+public class ETicaretContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Data Source=DEIMOS-CM\\SQLEXPRESS;Database=EFCoreGencay;Trusted_Connection=True;Trust Server Certificate=True;");
+    }
+}
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+}
